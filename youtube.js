e7f79a5af0f63af7d45ab1345def4d50fd178ecf
Ziv Levy <zivl@users.noreply.github.com>
2020-11-28 21:41:56 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -8,8 +8,8 @@ const puppeteer = require('puppeteer')
   // const context = await browser.createIncognitoBrowserContext();
   const page = await browser.newPage()
   await page.goto('https://youtu.be/Fqa32bZVpu4')
-  //      await new Promise(resolve => setTimeout(resolve, 1800000))
-  await new Promise((resolve) => setTimeout(resolve, 120000))
+  await new Promise(resolve => setTimeout(resolve, 1800000))
+//   await new Promise(resolve => setTimeout(resolve, 120000))
   await page.screenshot({path: './screenshot.png'});
   await browser.close()
 })()
