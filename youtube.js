b8644eaff59dfaaf1a2087df07fa49b6d602dd42
Ziv Levy <levi.ziv@gmail.com>
2020-11-28 10:15:19 +0200

    fix path

--- youtube.js
+++ youtube.js
@@ -10,6 +10,6 @@ const puppeteer = require('puppeteer')
   await page.goto('https://youtu.be/Fqa32bZVpu4')
   //      await new Promise(resolve => setTimeout(resolve, 1800000))
   await new Promise((resolve) => setTimeout(resolve, 120000))
-  await page.screenshot({path: './dist/screenshot.png'});
+  await page.screenshot({path: './screenshot.png'});
   await browser.close()
 })()
