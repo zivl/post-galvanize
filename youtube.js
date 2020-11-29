3ab61429af8cce742fd76b7ee7bdaf8bdb4c2a9b
Ziv Levy <levi.ziv@gmail.com>
2020-11-29 14:48:34 +0200

    reorder

--- youtube.js
+++ youtube.js
@@ -2,11 +2,6 @@ const puppeteer = require('puppeteer')
 
 const MINUTE = 60000
 
-function applyTimers(resolve) {
-  setInterval(() => console.log('still watching...'), 2 * MINUTE)
-  setTimeout(resolve, 10 * MINUTE)
-}
-
 ;(async () => {
   const browser = await puppeteer.launch({ headless: false })
   // const browser = await puppeteer.launch({headless: false,args: [
@@ -15,7 +10,10 @@ function applyTimers(resolve) {
   // const context = await browser.createIncognitoBrowserContext();
   const page = await browser.newPage()
   await page.goto('https://youtu.be/Fqa32bZVpu4')
-  await new Promise(applyTimers)
-  await page.screenshot({path: './screenshot.png'});
+  await new Promise((resolve) => {
+    setTimeout(resolve, 10 * MINUTE)
+    setInterval(() => console.log('still watching...'), 2 * MINUTE)
+  })
+  await page.screenshot({ path: './screenshot.png' })
   await browser.close()
 })()
